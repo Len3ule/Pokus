b8bbd5c1a79c2158e6fc07468ce8ac6a86932a9b
Lendule <lenka.zavitkovska@gmail.com>
2017-06-30 15:07:33 +0200

    zahíjení prpojektu

---
+++ src/Pokus.java
@@ -0,0 +1,9 @@
+/**
+ * Created by LeM on 30.06.2017.
+ */
+public class Pokus {
+    public static void main(String[] args) {
+        System.out.println("Moje bába");
+
+    }
+}
---
+++ src/ss.java
@@ -0,0 +1,5 @@
+/**
+ * Created by LeM on 30.06.2017.
+ */
+public class ss {
+}
